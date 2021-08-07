12ae18339cdda3ce09c07d028c5f8d8b3614425a
Asim <asim.awan@outlook.com>
2021-08-07 01:52:03 +0000

    created one object to store store all properties of task instead of several arrays to store different value

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 // Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-const taskDescription = [];
+// const taskTitles = [];
+// const taskComplete = [];
+// const taskDescription = [];
+
+
+
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
 function newTask(title, description) {
-  taskTitles.push(title);
-  taskComplete.push(false);
-  taskDescription.push(description);
+  const task = {
+    title: title,
+    description: description,
+    complete: false
+  };
+
+  return task;
 }
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
@@ -31,3 +38,9 @@ newTask("Do Laundry"); // task 1
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
 logTaskState(0); // Clean Cat Litter has been completed
+
+const task1 = newTask("Clean cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨")
+const tasks = [task1, task2];
+
+console.log(tasks);
